32766cc8d4d16acff10987ca862b3ce75b955f13
Brad Roberts <brad@bradroberts.net>
2017-10-24 10:53:32 -0400

    Event feed for current students website so we can import them automatically.

--- craft/config/elementapi.php
+++ craft/config/elementapi.php
@@ -5,6 +5,48 @@ namespace Craft;
 return [
     'endpoints' => [
 
+        'api/student-events' => function () {
+
+            $category = craft()->elements->getCriteria(ElementType::Category)->slug('students')->first();
+
+            return [
+                'elementType' => ElementType::Entry,
+                'criteria'    => [
+                    'section'   => 'events',
+                    'with'      => ['eventImageTall', 'eventImageWide'],
+                    'relatedTo' => $category,
+                ],
+                'transformer' => function (EntryModel $entry) {
+
+                    $description = $entry->html . '';
+
+                    if (!empty($entry->actions)) {
+                        $description .= '<p>';
+                        foreach ($entry->actions as $action) {
+                            $description .= '<a class="Button" href="' . $action->theUrl . '">' . $action->text . '</a>';
+                        }
+                        $description .= '</p>';
+                        $description = str_replace('<p></p>', '', $description);
+                    }
+
+                    return [
+                        'slug'        => $entry->slug,
+                        'title'       => $entry->title,
+                        'subtitle'    => $entry->dateRange,
+                        'excerpt'     => $entry->excerpt,
+                        'description' => $description,
+                        'url'         => $entry->theUrl,
+                        'image_tall'  => !empty($entry->eventImageTall) ? $entry->eventImageTall[0]->url : null,
+                        'image_wide'  => !empty($entry->eventImageWide) ? $entry->eventImageWide[0]->url : null,
+                        'publish_at'  => $entry->postDate->format('Y-m-d 00:00:00'),
+                        'expire_at'   => $entry->expiryDate ? $entry->expiryDate->format('Y-m-d 00:00:00') : null,
+                        'sort'        => 1,
+                    ];
+                },
+            ];
+
+        },
+
         'api/volunteer-skills' => function () {
 
             return [
