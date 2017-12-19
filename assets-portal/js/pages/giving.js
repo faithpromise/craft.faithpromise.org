d547b5bb6bfbc49f839a873edf95235e28bc8dc8
Brad Roberts <brad@bradroberts.net>
2017-12-19 14:57:21 -0500

    Trying to fix jquery error "Cannot set property 'mask' of undefined"

--- assets-portal/js/pages/giving.js
+++ assets-portal/js/pages/giving.js
@@ -1,37 +1,32 @@
 import user from '../common/user';
 import contentElement from '../common/content-element';
 
+let online_giving_pages = {
+    'giving':           { path: '/donate/index', login_required: true, },
+    'giving/profile':   { path: '/donate/profile', login_required: true, },
+    'giving/recurring': { path: '/donate/recurring', login_required: true, },
+    'giving/guest':     { path: '/donate/guest_donate', login_required: false, },
+};
+
 export default {
 
     init(page) {
 
         let online_giving_page = online_giving_pages[page];
 
-        // requireLogin(online_giving_page);
+        if (online_giving_page.login_required && !user.isLoggedIn())
+            return window.location.href = '/portal/login.aspx?ReturnUrl=' + '/portal/' + page;
+
         embedOnlineGiving(online_giving_page);
     }
 
 }
 
-let online_giving_pages = {
-    'giving':           '/donate/index',
-    'giving/profile':   '/donate/profile',
-    'giving/recurring': '/donate/recurring',
-    'giving/guest':     '/donate/guest_donate',
-};
-
-function requireLogin(page) {
-    if (!user.isLoggedIn())
-        window.location.href = '/portal/login.aspx?ReturnUrl=' + '/portal/' + page;
-}
-
 function embedOnlineGiving(og_page) {
-
-    // Embed Online Giving
     let js = document.createElement('script');
     js.setAttribute('data-id', 'og-embed-script');
     js.setAttribute('data-church-code', 'faithpromise_org');
-    js.setAttribute('data-default-page', og_page);
+    js.setAttribute('data-default-page', og_page.path);
     js.setAttribute('data-token-type', 'mp-token');
     js.setAttribute('data-mp-token', window.fp.MPP_User_Data);
     js.setAttribute('data-bg-color', '#f9f9f9');
